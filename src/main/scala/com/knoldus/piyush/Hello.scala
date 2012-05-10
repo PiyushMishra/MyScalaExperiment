0d8da323e52fa4998b6f15e48dda1af460d8b74c
Piyush Mishra <piyush@knoldus.com>
2012-05-10 10:42:15 +0530

    Hello world example

--- src/main/scala/com/knoldus/piyush/Hello.scala
+++ src/main/scala/com/knoldus/piyush/Hello.scala
@@ -1,5 +1,36 @@
 package com.knoldus.piyush
 
+class Hello
+{
+  
+def calculatesum(x:Int,y:Int):Int={x + y}  
+
+}
+
+
+
 object Hello extends App {
-	println("Hello")
+  
+  
+ val a:Int= args(0).toInt
+ val b:Int = args(1).toInt
+ val h=new Hello()
+ val hw= new SayHelloAgain()
+ val c:Int= h.calculatesum(a,b)
+println(c) 
+	hw.hello("Hello Again")
+	
+	
+	
+	
+}
+
+
+class SayHelloAgain
+{
+  
+def hello(S:String)={ println(S)}
+
+
+
 }
